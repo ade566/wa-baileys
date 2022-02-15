7d55d527fa1ef5f4493baaecabd2c2fddcfd9241
Muhamad Ade Rohayat <muhamadaderohayat122@gmail.com>
2022-02-15 13:37:50 +0700

    setup express for sendMessage

--- index.ts
+++ index.ts
@@ -1,9 +1,19 @@
 import { Boom } from '@hapi/boom'
 import { unlink } from 'fs';
 import P from 'pino'
+import * as express from 'express';
+import * as http from 'http'
 import makeWASocket, { DisconnectReason, useSingleFileAuthState } from '@adiwajshing/baileys'
 
+const port = 3000;
+const app = express();
+const server = http.createServer(app);
 var sock = {};
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 
 const startSock = async (id) => {
 	console.log(`ObjectID: ${id}`);
@@ -39,4 +49,26 @@ const startSock = async (id) => {
 var idClient = ["wacs3"]
 idClient.forEach(e => {
 	startSock(e)
+});
+
+app.post('/send-message', async (req, res) => {
+	const sender = req.body.sender;
+  const number = `${req.body.number}@s.whatsapp.net`;
+  const message = req.body.message;
+	try {
+		const sendMessage = await sock[sender].sendMessage(number, { text: message })
+		res.status(200).json({
+			status: true,
+			response: sendMessage
+		});
+	} catch (error) {
+		res.status(500).json({
+      status: false,
+      response: error,
+    });
+	}
+});
+
+server.listen(port, function () {
+  console.log(`http://localhost:${port}/?id=123&as=Ares`);
 });
